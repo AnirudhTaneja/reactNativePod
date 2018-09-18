45da296d12b69ad9ffa758d536d74f31cfe42506
AT <anirudhtaneja@live.com>
2018-09-17 19:49:37 -0500

    lib

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, Image,TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image,TextInput,ScrollView } from 'react-native';
 
 
 class Greeting extends React.Component {
@@ -41,7 +41,7 @@ export default class App extends React.Component {
         <View style={styles.buttonContainer}>
           <Button
             onPress={this._onPressButton}
-            title="Press Me"
+            title="Hello here"
           />
         </View>
       </View>
